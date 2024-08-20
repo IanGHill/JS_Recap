75bcef0cb8ef6702070c590c9645e154202d6b60
Ian Hill <ian.g.hill@outlook.com>
2024-08-20 12:44:06 +0100

    Operators

--- hello_world.js
+++ hello_world.js
@@ -1,1 +1,31 @@
-console.log('Hello world')
+console.log('Hello world');
+
+//Can add formatting if outputting in the browser console e.g.:
+// console.log("%cHello, World", "color: blue; font-size 40px");
+
+console.log("Hello", "World");
+
+var person = 'John';
+
+console.log("Hello", person);
+
+var greeting = "Hello"
+
+console.log(greeting, person);
+
+console.log('hello'.toUpperCase())
+
+console.log(typeof greeting)
+
+greeting = 5
+console.log(typeof greeting)
+
+//string interpolation
+greeting = 'Goodbye'
+console.log(`${greeting}, ${person}`)
+
+//string addition
+console.log(greeting + ' ' + person)
+
+var myVar;
+console.log(typeof myVar) // undefined
---
+++ operators.js
@@ -0,0 +1,17 @@
+console.log(2 + 2); //4
+console.log(2 + 2 + 2); //6
+console.log(20 - 18); //2
+console.log(2 * 3); //6
+console.log(2 ** 3); //8
+console.log(8 / 2); //4
+console.log(9 % 2); //1
+console.log(3 > 2); //true
+console.log(2 > 3); //false
+console.log(10 == 10); //true
+console.log(10 == '10'); //true
+console.log(10 === '10'); //false
+console.log(10 !== '10'); //true
+console.log(5 > 4 && 3 > 2); //true
+console.log(5 > 6 || 3 > 2); //true
+console.log(!(6<5)); //true
+console.log(5.4 % 3); //2.4
